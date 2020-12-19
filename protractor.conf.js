66ca129c4fefff3d851dad4fe918df2c21379511
Sergey <sergey.developer.it@gmail.com>
2020-12-19 05:24:56 +0000

    add e2e test cases framework

---
+++ protractor.conf.js
@@ -0,0 +1,19 @@
+exports.config = {
+  framework: 'jasmine',
+  specs: ['tests/e2e/*.spec.js'],
+  directConnect: true,
+  jasmineNodeOpts:
+  {
+      isVerbose: false,
+      showColors: true,
+      includeStackTrace: true,
+      defaultTimeoutInterval: 600000,
+  },
+  baseUrl: 'http:
+  capabilities: {
+    browserName: 'chrome',
+    chromeOptions: {
+      args: [ "--disable-gpu", "disable-infobars", "--window-size=1680,1050" ]
+    }
+  },
+}
