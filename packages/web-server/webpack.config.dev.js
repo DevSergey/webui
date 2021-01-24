87ec031e3b2ddf6fca8e4723de653f568148763b
Sergey <sergey.developer.it@gmail.com>
2021-01-24 17:08:21 +0000

    eslint fix

--- packages/web-server/webpack.config.dev.js
+++ packages/web-server/webpack.config.dev.js
@@ -3,9 +3,9 @@ const baseConfig = require('./webpack.config.base');
 const webpackDevConfig = baseConfig;
 webpackDevConfig.devtool = 'cheap-module-eval-srouce-map';
 webpackDevConfig.plugins.push(
-  new webpack.HotModuleReplacementPlugin(),
+  new webpack.HotModuleReplacementPlugin()
 );
 webpackDevConfig.entry.main.push(
-  'webpack-hot-middleware/client?reload=true',
+  'webpack-hot-middleware/client?reload=true'
 );
 module.exports = webpackDevConfig;
